bed6e90c73be340bd2c9efd11176a0bd3c59b363
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-21 18:05:57 +0200

    done

--- DownloadWebContent/ViewController.swift
+++ DownloadWebContent/ViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var webMy: UIWebView!
+    
+    let url = URL(string: "https://www.youtube.com/")!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let request = URLRequest(url:url)
+        //just show webcontent
+        webMy.loadRequest(request)
+       
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data,response,error in
+            if error != nil {
+                print(error)
+            } else {
+                if let unwrappedData = data {
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+
+print(dataString)
+                    
+                }
+            }
+        }
+        task.resume()
     }
 
 
